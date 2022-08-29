9938fb4a6d63a60557a0aee8edbee6e205786ffa
Omprakash Jaiswal <r.omprakash.jaiswal@accenture.com>
2022-08-29 16:09:18 +0530

    select input

--- src/QrScannerModal.tsx
+++ src/QrScannerModal.tsx
@@ -17,33 +17,34 @@ const Qrscan = () => {
     let qrScanner: QrScanner | null = null;
     const [camList, setCamList] = useState<any[]>([]);
 
+    const getCameras = async() => {
+        const camList: ((prevState: never[]) => never[]) | { id: string; label: string; }[] =[];
+        const cameras = await QrScanner.listCameras(true);
+        cameras.forEach((camera,index) => {
+            camList.push({
+                id: camera.id,
+                label: camera.label,
+            });
+
+            if(index === cameras.length-1) {
+                setCamList(camList);
+                alert(JSON.stringify(camList));
+
+            }  
+        });
+    };
+
   useEffect(() => {
     const videoElem = document.getElementById("qr-scan-window");
     if(videoElem) {
-        
+        const cameraList = getCameras();
         qrScanner = new QrScanner(
             videoElem as HTMLVideoElement,
             (result: any) => console.log('decoded qr code:', result),
             { /* your options or returnDetailedScanResult: true if you're not specifying any other options */ },
         );
         
-        qrScanner.start().then(() => {
-            const camList: ((prevState: never[]) => never[]) | { id: string; label: string; }[] =[];
-            QrScanner.listCameras(true).then(cameras => cameras.forEach((camera,index) => {
-                camList.push({
-                    id: camera.id,
-                    label: camera.label,
-                });
-
-                if(index === cameras.length-1) {
-                    setCamList(camList);
-                    alert(JSON.stringify(camList));
-
-                }
-
-                
-            }));
-        });
+        qrScanner.start();
     }
 
 
@@ -55,7 +56,9 @@ const Qrscan = () => {
         
     }
   }, [])
-
+  const onCameraChange = (event: { target: { value: string; }; }) => {
+    qrScanner?.setCamera(event?.target.value);
+  };
   return (
     <>
         <GlobalStyle/>
@@ -63,7 +66,11 @@ const Qrscan = () => {
         <h1> QrScanner</h1>
         <video id = "qr-scan-window"></video>
 
-        
+        <select onChange={onCameraChange} >
+            {camList.map((cam: any) => {
+                return (<option value={cam.id}>{cam.label}</option>)
+            })}
+        </select>
         </Wrapper>
     </>
   );
